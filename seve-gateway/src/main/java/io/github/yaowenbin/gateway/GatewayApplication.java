0dc5e09af9bbbf0a18aef12f3792ef5a12b54839
yao-wenbin <ywb992134@163.com>
2023-07-30 22:21:49 +0800

    feat: gateway module build.

---
+++ seve-gateway/src/main/java/io/github/yaowenbin/gateway/GatewayApplication.java
@@ -0,0 +1,15 @@
+package io.github.yaowenbin.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author yaowenbin
+ * @Date 2023/7/30
+ */
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String... args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
